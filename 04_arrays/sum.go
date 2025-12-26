61d4aa6c1f8353db0b84af6423bdf7f1c314bbdb
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2025-12-26 12:50:37 +0100

    feat(arrays): implement SumAllTails to pass tests

--- 04_arrays/sum.go
+++ 04_arrays/sum.go
@@ -15,3 +15,12 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	sums := []int{}
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return sums
+}

09e75d18142bd8ac4f980061d764cbff661fb009
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2025-12-29 09:15:15 +0100

    feat(structs): add Area

--- 05_structs/shapes_test.go
+++ 05_structs/shapes_test.go
@@ -10,3 +10,12 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(4.0, 3.0)
+	want := 12.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}

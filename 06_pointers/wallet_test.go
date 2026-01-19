a0196bb0ab5a3f8df986856f45120a391e5388f4
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2026-01-19 14:07:48 +0100

    test(pointers): add test for wallet (deposit amount, get balance)

---
+++ 06_pointers/wallet_test.go
@@ -0,0 +1,20 @@
+package pointers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	fmt.Printf("address of balnce in test is %p\n", &wallet.balance)
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
